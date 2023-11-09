2432f4c7fcab4b9fe00ee4bc63e8f76866a3889e
Rafael Abreu Fonseca <rafael.abreu.fonseca@gmail.com>
2023-11-09 20:50:09 +0100

    Insert test to favicon

--- musics/base/tests/test_home.py
+++ musics/base/tests/test_home.py
@@ -20,3 +20,8 @@ def test_title(response):
 
 def test_home_link(response):
     assert_contains(response, f'href="{reverse('base:home')}">Music Collection</a>')
+
+
+def test_favicon(response):
+    assert_contains(response, f"img/favicon.png")
+
