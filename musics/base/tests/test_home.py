3afbe76d51711a3acb9ab8458c7479ecdaaeddc2
Rafael Abreu Fonseca <rafael.abreu.fonseca@gmail.com>
2023-11-09 17:23:27 +0100

    Set and insert first frontend test

--- musics/base/tests/test_home.py
+++ musics/base/tests/test_home.py
@@ -1,6 +1,13 @@
 from django.test import Client
 
+from musics.django_assertions import assert_contains
 
-def test_status_code(client:Client):
+
+def test_status_code(client: Client):
     response = client.get('/')
     assert response.status_code == 200
+
+
+def test_title(client: Client):
+    response = client.get('/')
+    assert_contains(response, '<title>Music Collection</title>')
