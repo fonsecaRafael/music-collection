3bd28f97384e25e5921afbad98d0bc0418bc398f
Rafael Abreu Fonseca <rafael.abreu.fonseca@gmail.com>
2023-11-09 17:32:23 +0100

    Adds test_home link
    Test if one of the links on NavBar go to the home page.

--- musics/base/tests/test_home.py
+++ musics/base/tests/test_home.py
@@ -11,3 +11,8 @@ def test_status_code(client: Client):
 def test_title(client: Client):
     response = client.get('/')
     assert_contains(response, '<title>Music Collection</title>')
+
+
+def test_home_link(client: Client):
+    response = client.get('/')
+    assert_contains(response, 'href="/">Music Collection</a>')
