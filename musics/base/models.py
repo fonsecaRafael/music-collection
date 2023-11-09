0b00b3318a13a5a4b3c7c847735bb515728f817a
Rafael Abreu Fonseca <rafael.abreu.fonseca@gmail.com>
2023-11-09 04:03:07 +0100

    Finish changes in user model
    It creates the UserManager, this creates the user in the DB

--- musics/base/models.py
+++ musics/base/models.py
@@ -1,8 +1,41 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, full_name, username, password, **extra_fields):
+        """
+        Create and save a user with the given full_name, username, and password.
+        """
+        if not username:
+            raise ValueError("The given username must be set")
+        if not full_name:
+            raise ValueError("The given full name must be set")
+        user = self.model(full_name=full_name, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, full_name, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(full_name, username, password, **extra_fields)
+
+    def create_superuser(self, full_name, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(full_name, username, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -30,7 +63,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     )
     date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
 
-    # objects = UserManager()
+    objects = UserManager()
 
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = ["full_name", "password"]
