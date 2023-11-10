3026f2658d03b1d8ee6663f96935c0aa3388f176
Rafael Abreu Fonseca <rafael.abreu.fonseca@gmail.com>
2023-11-11 00:51:53 +0100

    Make artists list test pass
    Just create an artist list page, need refactor

--- musics/artists/urls.py
+++ musics/artists/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from musics.artists.views import artist
-
+from musics.artists.views import artist, artists
 
 app_name = 'artists'
 urlpatterns = [
     path('<artist_id>', artist, name='artist'),
+    path('', artists, name='artists'),
 ]
--- musics/artists/views.py
+++ musics/artists/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def artist(request, artist_id):
     artist = {'id': artist_id}
     return render(request, 'artists/details.html', context={'artist': artist})
+
+
+def artists(request):
+    return render(request, 'artists/artists.html')
