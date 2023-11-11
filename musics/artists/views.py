a4dc61477d13165b3d84d32f7c6bbecf4a2e62fe
Rafael Abreu Fonseca <rafael.abreu.fonseca@gmail.com>
2023-11-11 05:00:04 +0100

    Refactor: Details page
    Finish

--- musics/artists/views.py
+++ musics/artists/views.py
@@ -2,15 +2,21 @@ import requests
 from django.shortcuts import render
 
 
+def send_get(url, headers):
+    return requests.get(url, headers=headers).json()
+
+
 def artist(request, artist_id):
-    artist = {'id': artist_id}
+    url = "https://europe-west1-madesimplegroup-151616.cloudfunctions.net/artists-api-controller?artist_id=" + artist_id
+    artist = send_get(url, {'Authorization': 'Basic ZGV2ZWxvcGVyOlpHVjJaV3h2Y0dWeQ =='})
+
     return render(request, 'artists/details.html', context={'artist': artist})
 
 
 def artists(request):
-    url = 'https://europe-west1-madesimplegroup-151616.cloudfunctions.net/artists-api-controller'
-    headers = {'Authorization': 'Basic ZGV2ZWxvcGVyOlpHVjJaV3h2Y0dWeQ =='}
-    response = requests.get(url, headers=headers)
-    artists = response.json()
+    artists = send_get(
+        'https://europe-west1-madesimplegroup-151616.cloudfunctions.net/artists-api-controller',
+        {'Authorization': 'Basic ZGV2ZWxvcGVyOlpHVjJaV3h2Y0dWeQ =='}
+    )
 
     return render(request, 'artists/artists.html', context={'response': artists})
