f45525d56f7159fe3c92ed13127ee53bbf38d231
Rafael Abreu Fonseca <rafael.abreu.fonseca@gmail.com>
2023-11-12 01:22:53 +0100

    Create Test that check value in DB

--- musics/albums/tests/test_albums_update.py
+++ musics/albums/tests/test_albums_update.py
@@ -1,10 +1,19 @@
 import pytest
 from django.urls import reverse
 
+from musics.albums.models import Album
+
 
 @pytest.fixture
-def response(client):
-    return client.get(reverse('albums:album'), args=('1',))
+def create_album(db):
+    album = Album(artist='Rihanna', name='Loud', year='2010-01-01')
+    album.save()
+    return album
+
+
+@pytest.fixture
+def response(client, create_album):
+    return client.get(reverse('albums:album', args=('1',)))
 
 
 def test_status_code(response):
