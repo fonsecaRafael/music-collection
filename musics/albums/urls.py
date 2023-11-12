61684b58f89adba1199e82dfc710481271e6298e
Rafael Abreu Fonseca <rafael.abreu.fonseca@gmail.com>
2023-11-12 01:23:45 +0100

    Makes test about value in DB pass
    Need refactor the template

--- musics/albums/urls.py
+++ musics/albums/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from musics.albums.views import albums
+from musics.albums.views import albums, album
 
 app_name = 'albums'
 urlpatterns = [
     path('', albums, name='albums'),
+    path('<album_id>', album, name='album')
 ]
--- musics/albums/views.py
+++ musics/albums/views.py
@@ -6,3 +6,8 @@ from musics.albums.models import Album
 def albums(request):
     albums = Album.objects.all()
     return render(request, 'albums/albums.html', context={'albums': albums})
+
+
+def album(request, album_id):
+    album = Album.objects.get(id=album_id)
+    return render(request, 'albums/album.html', context={'album': album})
