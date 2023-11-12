0ab909d6dd9c0fea33715b703fc4b0ac74834bd8
Rafael Abreu Fonseca <rafael.abreu.fonseca@gmail.com>
2023-11-12 04:52:17 +0100

    Adds create, update and delete Album views

--- musics/albums/views.py
+++ musics/albums/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from musics.albums.forms import AlbumForm
 from musics.albums.models import Album
 
 
+def create_album(request):
+    if request.method == "POST":
+        album_form = AlbumForm(request.POST)
+        if album_form.is_valid():
+            try:
+                album_form.save()
+                return redirect(Album)
+            except:
+                pass
+    else:
+        album_form = AlbumForm()
+    return render(request, 'albums/create_album.html', {'album_form': album_form})
+
+
+# Recovery
 def albums(request):
     albums = Album.objects.all()
     return render(request, 'albums/albums.html', context={'albums': albums})
@@ -11,3 +27,18 @@ def albums(request):
 def album(request, album_id):
     album = Album.objects.get(id=album_id)
     return render(request, 'albums/album.html', context={'album': album})
+
+
+def update_album(request, id):
+    album = Album.objects.get(id=id)
+    album_form = AlbumForm(request.POST, instance=album)
+    if album_form.is_valid():
+        album_form.save()
+        return redirect(Album)
+    return render(request, 'albums/update_album.html', {'album': album})
+
+
+def delete_album(request, id):
+    album = Album.objects.get(id=id)
+    album.delete()
+    return redirect(Album)
