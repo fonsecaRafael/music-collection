34b6fac3e5d731784352f5766e7405c32284256c
Rafael Abreu Fonseca <rafael.abreu.fonseca@gmail.com>
2023-11-12 04:59:50 +0100

    adds routing for albums CRUD

--- musics/albums/urls.py
+++ musics/albums/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 
-from musics.albums.views import albums, album
+from musics.albums.views import albums, album, create_album, update_album, delete_album
 
 app_name = 'albums'
 urlpatterns = [
+    path('create_album', create_album, name='create_album'),
     path('', albums, name='albums'),
-    path('<album_id>', album, name='album')
+    path('<album_id>', album, name='album'),
+    path('update/<id>', update_album, name='update_album'),
+    path('delete/<id>', delete_album, name='delete_album'),
 ]
+
+# from django.contrib import admin
+# urlpatterns = [
+# path('admin/', admin.site.urls),
+# ]
