0b2d46090f60cb63b50b5234222dc1b6fef5c9b2
Rafael Abreu Fonseca <rafael.abreu.fonseca@gmail.com>
2023-11-15 16:59:35 +0100

    Adds global admin logged client fixtures

--- musics/conftest.py
+++ musics/conftest.py
@@ -5,10 +5,23 @@ from model_mommy import mommy
 @pytest.fixture()
 def logged_user(db, django_user_model):
     user_model = mommy.make(django_user_model)
+    user_model.is_staff = False
+    return user_model
+
+
+@pytest.fixture()
+def logged_admin(db, django_user_model):
+    user_model = mommy.make(django_user_model, is_staff=True)
     return user_model
 
 
 @pytest.fixture()
 def logged_client(logged_user, client):
     client.force_login(logged_user)
+    return client
+
+
+@pytest.fixture()
+def admin_logged_client(logged_admin, client):
+    client.force_login(logged_admin)
     return client
