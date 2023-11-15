c8e9292afc237139240827c17fd256c39c6854fd
Rafael Abreu Fonseca <rafael.abreu.fonseca@gmail.com>
2023-11-15 16:59:10 +0100

    Tests to prevent non-admins from deleting albums

--- musics/albums/tests/test_albums_update.py
+++ musics/albums/tests/test_albums_update.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from model_mommy import mommy
 
 from musics.albums.models import Album
+from musics.django_assertions import assert_contains, assert_not_contains
 
 
 @pytest.fixture
@@ -12,9 +13,22 @@ def create_albums(db):
 
 
 @pytest.fixture
-def response(client, create_album):
-    return client.get(reverse('albums:album', args=('1',)))
+def resp_album_user_logged(logged_client, create_albums):
+    return logged_client.get(reverse('albums:album', args=('1',)))
+
+
+@pytest.fixture
+def resp_album_admin_logged(admin_logged_client, create_albums):
+    return admin_logged_client.get(reverse('albums:album', args=('1',)))
 
 
 def test_status_code(response):
     assert response.status_code == 200
+
+
+def test_user_logged_delete_unavailable(resp_album_user_logged):
+    assert_not_contains(resp_album_user_logged, reverse('albums:delete', args=[1]))
+
+
+def test_admin_logged_delete_available(resp_album_admin_logged):
+    assert_contains(resp_album_admin_logged, reverse('albums:delete', args=('1',)))
