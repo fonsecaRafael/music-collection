87f5bca0fc4c1d1e812291fcd8fc77decc244e25
Rafael Abreu Fonseca <rafael.abreu.fonseca@gmail.com>
2023-11-15 23:33:10 +0100

    Adds upgrade view and Refactor code

--- musics/base/urls.py
+++ musics/base/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from musics.base.views import home, sign_up, users
+from musics.base.views import home, sign_up, users, upgrade, downgrade
 
 
 app_name = 'base'
 urlpatterns = [
     path('', home, name='home'),
     path('signup/', sign_up, name='signup'),
-    path('users/', users, name='users')
+    path('users/', users, name='users'),
+    path('users/upgrade/<id>', upgrade, name='upgrade'),
+    path('users/<id>/downgrade', downgrade, name='downgrade'),
 ]
--- musics/base/views.py
+++ musics/base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+from musics.base.models import User
+
 
 def home(request):
     return render(request, 'base/home.html')
@@ -9,9 +11,28 @@ def sign_up(request):
     return render(request, 'base/sign_up.html')
 
 
+def only_admin_should_pass(request):
+    if request.user.is_authenticated and request.user.is_staff:
+        return True
+    return False
+
+
 def users(request):
-    if request.user.is_staff:
-        return render(request, 'base/users.html')
-    elif request.user.is_authenticated:
-        return redirect('base:home')
-    return redirect('login')
+    if only_admin_should_pass(request):
+        users = User.objects.exclude(id=request.user.id)
+        return render(request, 'base/users.html', context={'users': users})
+    return redirect('base:home')
+
+
+def upgrade(request, id):
+    if only_admin_should_pass(request):
+        user = User.objects.get(id=id)
+        user.is_staff = True
+        user.save()
+        return redirect('base:users')
+    return redirect('base:home')
+
+
+
+def downgrade(request, user_id):
+    return render(request, 'base/sign_up.html')
