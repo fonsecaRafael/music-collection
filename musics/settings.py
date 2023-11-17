d1c8bc3234a86475c5284d3ad3fdf472495cd40f
Rafael Abreu Fonseca <rafael.abreu.fonseca@gmail.com>
2023-11-17 18:49:39 +0100

    Adds class in Django message_tags

--- musics/settings.py
+++ musics/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 
 import dj_database_url
 from decouple import config, Csv
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
