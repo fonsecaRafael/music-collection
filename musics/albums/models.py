edf1867f05faa638a74d4509441932e0896d5287
Rafael Abreu Fonseca <rafael.abreu.fonseca@gmail.com>
2023-11-17 01:49:02 +0100

    Refactor: Limit values to years

--- musics/albums/models.py
+++ musics/albums/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.urls import reverse
 
@@ -5,7 +6,10 @@ from django.urls import reverse
 class Album(models.Model):
     artist = models.CharField(max_length=32)
     name = models.CharField(max_length=32)
-    year = models.IntegerField()
+    year = models.IntegerField(validators=[
+            MaxValueValidator(2050),
+            MinValueValidator(1600)
+        ])
 
     def get_absolute_url(self):
         return reverse('albums:albums')
